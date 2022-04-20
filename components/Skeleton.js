528cc21a5fa2485305be70600b6c581978280700
thisiskas <dimowookasime@gmail.com>
2022-04-20 21:18:40 +0200

    modify

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -8,27 +8,29 @@ export default function Skeleton() {
       <div className="s-content"></div>
 
       <style jsx>{`
-        .skeleton {
-          max-width: 1200px;
-          margin: 20px auto;
-        }
-        .skeleton > div {
-          background: #dbcc1a;
-          border-radius: 4px;
-          margin: 20px 0;
-        }
-        .s-banner {
-          padding: 12% 0;
-        }
-        .s-header {
-          padding: 15px 0;
-          max-width: 500px;
-        }
-        .s-content {
-          padding: 8px 0;
-          max-width: 1000px;
+        @media only screen and (min-width: 600px) {
+          .skeleton {
+            max-width: 1200px;
+            margin: 20px auto;
+          }
+          .skeleton > div {
+            background: #dbcc1a;
+            border-radius: 4px;
+            margin: 20px 0;
+          }
+          .s-banner {
+            padding: 12% 0;
+          }
+          .s-header {
+            padding: 15px 0;
+            max-width: 500px;
+          }
+          .s-content {
+            padding: 8px 0;
+            max-width: 1000px;
+          }
         }
       `}</style>
     </div>
-  )
+  );
 }
