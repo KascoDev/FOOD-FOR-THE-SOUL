6edb537c2ac38baba3eb42fd7da38bf6284b23f1
thisiskas <dimowookasime@gmail.com>
2022-04-20 14:28:58 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This is the Next.js starter site (and course files) for the Next.js & Contentful tutorial by the Net Ninja.
+
+## Getting Started
+
+To use the starter project, run the following in a terminal:
+
+```bash
+
+```
---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <header>
+        <Link href="/">
+          <a>
+            <h1>
+              <span>Food for the soul</span>
+            </h1>
+            <h2>Spread The Joy</h2>
+          </a>
+        </Link>
+      </header>
+
+      <div className="page-content">{children}</div>
+
+      <footer>
+        <p>Copyright 2022 Food for the soul</p>
+      </footer>
+    </div>
+  );
+}
